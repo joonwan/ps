04565dc4654ddfd1f702afd13d4dd785560dff0f
joonwan <joonwan1024@naver.com>
2025-02-17 23:45:53 +0900

    prog: 두 원 사이의 정수 쌍

---
+++ programmers/level/2/두 원 사이의 정수 쌍/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long solution(int r1, int r2) {
+        long answer = 0;
+
+        for (int i = 0; i < r2; i++) {
+            if (i < r1) {
+                answer += Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2))) - Math.ceil(Math.sqrt(Math.pow(r1, 2) - Math.pow(i, 2))) + 1;
+            } else {
+                answer += Math.floor(Math.sqrt(Math.pow(r2, 2) - Math.pow(i, 2)));
+            }
+        }
+
+        return answer * 4;
+    }
+}
